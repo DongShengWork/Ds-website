68d5b5266547b713d2163c78efde1625a0c46e8d
lighthouse-8G <lighthouse@8g.com>
2023-07-10 14:51:54 +0800

    fixed

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,3 +1,66 @@
+import ReactFullpage from '@fullpage/react-fullpage';
+import config from './config';
+import './index.less';
+
 export default () => {
-  return <div>个人主页</div>
-}
+  return (
+    <div className="app-pages">
+      <ReactFullpage
+        credits={{
+          enabled: false,
+        }}
+        //fullpage options
+        scrollingSpeed={600} /* Options here */
+        navigation
+        verticalCentered
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              {config.map((item) => {
+                return (
+                  <div className="section" key={item.title}>
+                    <div className="main">
+                      <div className="desc">
+                        <a href={item.href} target="_blank">
+                          {item.title}
+                          &nbsp;&nbsp;&nbsp;
+                          <svg
+                            className="icon"
+                            viewBox="0 0 1024 1024"
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            p-id="7821"
+                            width="12"
+                            height="12"
+                          >
+                            <path
+                              d="M924.402464 1023.068211H0.679665V99.345412h461.861399v98.909208H99.596867v725.896389h725.896389V561.206811h98.909208z"
+                              p-id="7822"
+                              fill='#fff'
+                            ></path>
+                            <path
+                              d="M930.805104 22.977336l69.965436 69.965436-453.492405 453.492404-69.965435-69.901489z"
+                              p-id="7823"
+                              fill='#fff'
+                            ></path>
+                            <path
+                              d="M1022.464381 304.030081h-98.917201V99.345412H709.230573V0.428211h313.233808z"
+                              p-id="7824"
+                              fill='#fff'
+                            ></path>
+                          </svg>
+                        </a>
+                      </div>
+                      <img src={item.src} />
+                    </div>
+                  </div>
+                );
+              })}
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
+      <div className="beian">浙ICP备2013019046</div>
+    </div>
+  );
+};
