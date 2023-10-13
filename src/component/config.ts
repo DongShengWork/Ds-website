b9ff14e437a828072a547625b61a7e87750a3809
lighthouse-8G <lighthouse@8g.com>
2023-10-13 19:00:29 +0800

    fixed

--- src/component/config.ts
+++ src/component/config.ts
@@ -2,41 +2,41 @@ export default [
   {
     title: 'React基础组件库',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/yl-design.png',
-    href: 'http://server.yunliang.cloud/website/yl-design',
+    href: 'http://dev-ops.yunliang.cloud/website/yl-design',
   },
   {
     title: 'React配置化组件库',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/react-core-form.png',
-    href: 'http://server.yunliang.cloud/website/react-core-form',
+    href: 'http://dev-ops.yunliang.cloud/website/react-core-form',
   },
   {
     title: '前端基础工具库',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/react-core-form-tools.png',
-    href: 'http://server.yunliang.cloud/website/react-core-form-tools',
+    href: 'http://dev-ops.yunliang.cloud/website/react-core-form-tools',
   },
   {
     title: '低代码相关组件',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/react-core-form-designer.png',
-    href: 'http://server.yunliang.cloud/website/react-core-form-designer',
+    href: 'http://dev-ops.yunliang.cloud/website/react-core-form-designer',
   },
   {
     title: '开发套件后台管理',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/my-dev-tools.png',
-    href: 'http://server.yunliang.cloud/website/my-dev-tools',
+    href: 'http://dev-ops.yunliang.cloud/website/my-dev-tools',
   },
   {
     title: '在线轻组件服务',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/code-playground.png',
-    href: 'http://server.yunliang.cloud/website/code-playground',
+    href: 'http://dev-ops.yunliang.cloud/website/code-playground',
   },
   {
     title: '中后台CRUD模型',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/crud-model.png',
-    href: 'http://server.yunliang.cloud/website/crud-model',
+    href: 'http://dev-ops.yunliang.cloud/website/crud-model',
   },
   {
     title: '多应用统一登录',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/login.png',
-    href: 'http://server.yunliang.cloud/website/unification-login',
+    href: 'http://dev-ops.yunliang.cloud/website/unification-login',
   },
 ];
