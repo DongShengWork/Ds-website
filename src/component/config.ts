f016054407c28e13d6e899c22e8b80a09a764926
lighthouse-8G <lighthouse@8g.com>
2023-10-13 19:26:34 +0800

    fixed

--- src/component/config.ts
+++ src/component/config.ts
@@ -20,11 +20,6 @@ export default [
     href: 'http://dev-ops.yunliang.cloud/website/react-core-form-designer',
   },
   {
-    title: '开发套件后台管理',
-    src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/my-dev-tools.png',
-    href: 'http://dev-ops.yunliang.cloud/website/my-dev-tools',
-  },
-  {
     title: '在线轻组件服务',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/code-playground.png',
     href: 'http://dev-ops.yunliang.cloud/website/code-playground',
@@ -37,6 +32,16 @@ export default [
   {
     title: '多应用统一登录',
     src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/login.png',
-    href: 'http://dev-ops.yunliang.cloud/website/unification-login',
+    href: 'http://ulp.yunliang.cloud',
+  },
+  {
+    title: '统一中台管理',
+    src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/center.png',
+    href: 'http://center.yunliang.cloud',
+  },
+  {
+    title: '开发套件后台管理',
+    src: 'https://my-dev-ops.oss-cn-beijing.aliyuncs.com/website/my-dev-tools.png',
+    href: 'http://dev-ops.yunliang.cloud',
   },
 ];
