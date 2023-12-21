5bfd9be42061899441e3305b6a079a8e642f5027
xulei <xulei@lei.com>
2023-12-21 21:19:22 +0800

    fixed

--- src/components/advance.tsx
+++ src/components/advance.tsx
@@ -1,25 +1,10 @@
-import { Tooltip } from "@arco-design/web-react";
+import { Button } from '@arco-design/web-react';
 
-export default ({
-  data = []
-}) => {
+export default ({ data = [] }) => {
   return (
     <div className="app-advance">
       {data.map((menu: any) => {
-        return (
-          <Tooltip content={menu.title} position="top">
-            <div
-              key={menu.title}
-              className="app-advance-item"
-              onClick={() => {
-                window.open(menu.href);
-              }}
-            >
-              <img src={menu.icon} style={{ width: menu.width || 50, height: 50 }} />
-              <span>{menu.title}</span>
-            </div>
-          </Tooltip>
-        );
+        return <Button key={menu.title}>{menu.title}</Button>;
       })}
     </div>
   );
